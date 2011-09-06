2680dd1676e55ae02923a019d9bdc27860f72d46
Ian <ian.grabill@gmail.com>
2011-09-06 00:49:38 -0400

    first commit

---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ActiveRecord::Base
+  
+  acts_as_list
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Books::Application.config.secret_token = '6eb103f3e24bb80a10f96aea3d8addfb200525959d24274ddfdc1270605cb7d25c46ca6ac08404ff561413bcc2dc6dab9fa79ae9199e59781d94e08eae291e0f'
