f31b87cd24e633c4099a0804cc40ff7286fbcca2
Ian <ian.grabill@gmail.com>
2011-09-07 21:12:21 -0400

    checked admin wall functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,7 +49,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to(@book, :notice => 'Book was successfully created.') }
+        format.html { redirect_to(root_path, :notice => 'Book was successfully created.') }
         format.xml  { render :xml => @book, :status => :created, :location => @book }
       else
         format.html { render :action => "new" }
