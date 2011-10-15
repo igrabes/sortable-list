4db16e74fd84f108b3879dcc03ec96e48fe7f943
Ian <ian.grabill@gmail.com>
2011-10-15 14:19:30 -0400

    fixed bug:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController  
   
  def new
-  if current_user.nil?
+   if current_user.nil?
     deny_access
   else
     @service = Service.new
     @title = "Create New Service"
+  end
  end                            
  
 end
