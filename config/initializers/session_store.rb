332f4e03ce5631feebaa8ffc3c24458ee61c085b
Ian <ian.grabill@gmail.com>
2011-10-30 16:39:10 -0400

    working on cookies issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Books::Application.config.session_store :cookie_store, :key => '_books_session'
+Books::Application.config.session_store :cookie_store, :key => '_books_session'   
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
